92f10509bb87946e55dd82855c8aca2960d1dda5
enzowxl <whayyzs13@gmail.com>
2024-09-28 21:03:39 -0300

    feat: add domains ins domainList

--- src/app/(dashboard)/dashboard/domains/page.tsx
+++ src/app/(dashboard)/dashboard/domains/page.tsx
@@ -1,7 +1,7 @@
 import { NavAuth } from '@/app/(dashboard)/_components/nav-auth'
 import { DomainList } from './_components/domain-list'
-// import { auth } from '@/auth'
-// import { prisma } from '@/lib/prisma'
+import { auth } from '@/auth'
+import { prisma } from '@/lib/prisma'
 
 export async function generateMetadata() {
   return {
@@ -10,11 +10,11 @@ export async function generateMetadata() {
 }
 
 const DomainsPage = async () => {
-  // const session = await auth()
+  const session = await auth()
 
-  // const domains = await prisma.domain.findMany({
-  //   where: { userId: session?.user.sub },
-  // })
+  const domains = await prisma.domain.findMany({
+    where: { userId: session?.user.sub },
+  })
 
   // const response = await fetch(
   //   'https://api.vercel.com/v9/projects/easylinks/domains',
@@ -30,7 +30,7 @@ const DomainsPage = async () => {
     <div className="flex flex-col">
       <NavAuth title="Domains" />
 
-      <DomainList domains={[]} />
+      <DomainList domains={domains} />
     </div>
   )
 }
