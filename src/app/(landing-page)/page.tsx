d2558eb8268a4b4e19d53a2e941eb7c8078ed959
enzowxl <whayyzs13@gmail.com>
2024-10-07 19:09:45 -0300

    remove: delete containerScroll

--- src/app/(landing-page)/page.tsx
+++ src/app/(landing-page)/page.tsx
@@ -1,6 +1,5 @@
 import { NavLandingPage } from './_components/nav-landing-page'
 import { BackgroundBeams } from '@/components/ui/background-beams'
-import { ContainerScroll } from '@/components/ui/container-scroll-animation'
 
 const LandingPage = () => {
   return (
@@ -8,17 +7,7 @@ const LandingPage = () => {
       <NavLandingPage />
 
       <section className="h-screen w-full bg-gradient-to-b from-dark to-neutrals-12 rounded-md !overflow-visible relative flex flex-col items-center  antialiased">
-        <div className="z-[1] flex flex-col mt-[-100px] md:mt-[-50px]">
-          <ContainerScroll
-            titleComponent={
-              <h1 className="pb-2 text-5xl md:text-8xl bg-clip-text text-transparent bg-gradient-to-b from-white to-purplePrimary font-bold">
-                Shorten your links with easylinks
-              </h1>
-            }
-          >
-            Waiting...
-          </ContainerScroll>
-        </div>
+        <div className="z-[1] flex flex-col mt-[-100px] md:mt-[-50px]"></div>
         <BackgroundBeams />
       </section>
     </main>
