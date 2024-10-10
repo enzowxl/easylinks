0f0e77257a638bee2a4e13cc910c32faf8cc002e
enzowxl <whayyzs13@gmail.com>
2024-10-09 21:07:15 -0300

    test: testing redirect link

--- src/app/(redirect)/[slug]/page.tsx
+++ src/app/(redirect)/[slug]/page.tsx
@@ -1,5 +1,57 @@
-const RedirectPage = () =>
-  // { params: { slug } }: { params: { slug: string } }
-  {}
+import { prisma } from '@/lib/prisma'
+import { headers } from 'next/headers'
+import { notFound } from 'next/navigation'
+
+const RedirectPage = async ({
+  params: { slug },
+}: {
+  params: { slug: string }
+}) => {
+  const headersList = headers()
+  const host = headersList.get('host')
+
+  const findDomainByHost = await prisma.domain.findUnique({
+    where: { domainName: host || undefined },
+  })
+
+  let findLinkBySlug
+
+  if (findDomainByHost) {
+    await prisma.link.findUnique({
+      where: {
+        slug,
+        domain: {
+          domainName: findDomainByHost.domainName,
+        },
+      },
+      include: {
+        domain: true,
+      },
+    })
+  } else {
+    findLinkBySlug = await prisma.link.findUnique({
+      where: { slug },
+      include: {
+        domain: true,
+      },
+    })
+  }
+
+  if (findLinkBySlug?.domain) {
+    if (findDomainByHost?.domainName !== host) {
+      return notFound()
+    }
+  }
+
+  if (!findLinkBySlug) return notFound()
+
+  return (
+    <div>
+      host: {host}
+      <pre>findLinkBySlug: {JSON.stringify(findLinkBySlug, null, 2)}</pre>
+      <pre>findDomainByHost: {JSON.stringify(findDomainByHost, null, 2)}</pre>
+    </div>
+  )
+}
 
 export default RedirectPage
