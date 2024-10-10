ba972b54ee52aabc4bd2848671b93721fed28430
enzowxl <whayyzs13@gmail.com>
2024-10-09 23:55:16 -0300

    test: add log in redirect link

--- src/app/(redirect)/[slug]/page.tsx
+++ src/app/(redirect)/[slug]/page.tsx
@@ -6,7 +6,7 @@ const RedirectPage = async ({
 }: {
   params: { slug: string }
 }) => {
-  let a
+  const log = []
   const headersList = headers()
   const host = headersList.get('host')
 
@@ -28,7 +28,7 @@ const RedirectPage = async ({
         domain: true,
       },
     })
-    a = 'o link tem dominio'
+    log.push('o link tem dominio')
   } else {
     findLinkBySlug = await prisma.link.findUnique({
       where: { slug },
@@ -36,25 +36,28 @@ const RedirectPage = async ({
         domain: true,
       },
     })
-    a = 'o link nao tem dominio'
+    log.push('o link nao tem dominio')
   }
 
   if (findLinkBySlug?.domain) {
     if (findDomainByHost?.domainName !== host) {
-      a = 'nao tem dominio igual'
+      log.push('nao tem dominio igual')
     }
   }
 
   if (!findLinkBySlug) {
-    a = 'nao achou o link'
+    log.push('nao achou o link')
   }
 
   return (
     <div>
-      host: {host}
+      <p>host: {host}</p>
+      <br />
       <pre>findLinkBySlug: {JSON.stringify(findLinkBySlug, null, 2)}</pre>
+      <br />
       <pre>findDomainByHost: {JSON.stringify(findDomainByHost, null, 2)}</pre>
-      <p>{a}</p>
+      <br />
+      <p>{log.join(', ')}</p>
     </div>
   )
 }
